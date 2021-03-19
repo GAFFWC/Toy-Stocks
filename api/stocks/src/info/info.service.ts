b8393a1efdac843acc50d7563f2ef44354422301
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-03-19 16:57:34 +0900

    주가 service 별도 추가

--- api/stocks/src/info/info.service.ts
+++ api/stocks/src/info/info.service.ts
@@ -46,3 +46,64 @@ export class InfoService {
 		}
 		return;
 	}
+
+	async getPrice(code: String): Promise<PriceInfoResponse> {
+		const URL = `https://finance.naver.com/item/main.nhn?code=${code}`;
+		const crawled = await this.http
+			.get(URL, {
+				responseType: "arraybuffer"
+			})
+			.toPromise()
+			.then((res) => {
+				if (res.status !== 200) {
+					return false;
+				}
+
+				const $ = cheerio.load(iconv.decode(res.data, "EUC-KR"));
+				return $(".blind")
+					.find("dd")
+					.text()
+					.split(" ")
+					.map((value) => {
+						return value.replace(/[^0-9]/g, "");
+					});
+			})
+			.catch((err) => {
+				console.log(err);
+				return false;
+			});
+
+		if (!crawled) {
+			return {
+				result: false
+			};
+		}
+
+		try {
+			const newPriceInfo = new PriceInfo();
+
+			newPriceInfo.datetime = new Date(crawled[0] + "-" + crawled[1] + "-" + crawled[2] + " " + crawled[3] + ":" + crawled[4] + ":00");
+			newPriceInfo.price = {
+				now: parseInt(crawled[10]),
+				yesterday: {
+					start: parseInt(crawled[17]),
+					highest: parseInt(crawled[18]),
+					lowest: parseInt(crawled[20]),
+					upperLimit: parseInt(crawled[19]),
+					lowerLimit: parseInt(crawled[21])
+				}
+			};
+
+			newPriceInfo.volume = parseInt(crawled[22]);
+
+			return {
+				result: true,
+				data: newPriceInfo
+			};
+		} catch (err) {
+			return {
+				result: false
+			};
+		}
+	}
+}
