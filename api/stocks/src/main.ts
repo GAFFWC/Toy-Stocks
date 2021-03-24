799687b17bb0808603a9d544977dd15cebed0cc6
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-03-24 18:02:22 +0900

    Update main.ts
    스웨거 세팅

--- api/stocks/src/main.ts
+++ api/stocks/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    // prefix : /api/v1
+    app.setGlobalPrefix('api/v1');
+
+    // swagger document creation
+    const document = SwaggerModule.createDocument(
+        app,
+        new DocumentBuilder()
+            .setTitle('Toy-Stocks API')
+            .setVersion('1.0')
+            .setDescription('Toy-Stocks API v1.0')
+            .build(),
+    );
+
+    // swagger setup
+    SwaggerModule.setup('api/v1/swagger', app, document, {
+        customSiteTitle: 'Toy-Stocks',
+        // customfavIcon:
+    });
+
+    await app.listen(3000);
 }
 bootstrap();
