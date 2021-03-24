a31c838eb7c54b547e0d152b68d240c1d4b10a5f
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-03-24 17:59:56 +0900

    entity 스웨거 설정

--- api/stocks/src/entities/company.entity.ts
+++ api/stocks/src/entities/company.entity.ts
@@ -1,67 +1,91 @@
-import { IsDate, IsEnum, IsNumberString, IsString, IsUrl } from "class-validator";
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDate, IsEnum, IsNumber, IsNumberString, IsString, IsUrl } from 'class-validator';
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { URL } from 'url';
 
 enum MarketType {
-	KOSPI,
-	KOSDAQ,
-	KONEX
+    KOSPI,
+    KOSDAQ,
+    KONEX,
 }
 
 @Entity()
 export class Company {
-	@PrimaryGeneratedColumn("increment", { type: "bigint", unsigned: true })
-	companyUid: number;
+    @ApiProperty({ description: 'Company 고유 ID' })
+    @PrimaryGeneratedColumn('increment', { type: 'bigint', unsigned: true })
+    companyUid: number;
 
-	// 시장 구분
-	@IsEnum(MarketType)
-	@Column({ type: "enum", enum: MarketType, nullable: false, comment: "시장 구분 (KOSPI, KOSDAQ, KONEX)" })
-	type: MarketType;
+    // 시장 구분
+    @ApiProperty({
+        description: '시장 구분 (KOSPI, KOSDAQ, KONEX)',
+        enum: MarketType,
+        example: '(KOSPI, KOSDAQ, KONEX)',
+    })
+    @IsEnum(MarketType)
+    @Column({ type: 'enum', enum: MarketType, nullable: false, comment: '시장 구분 (KOSPI, KOSDAQ, KONEX)' })
+    type: MarketType;
 
-	// 이름
-	@IsString()
-	@Column({ type: "varchar", nullable: false, comment: "이름" })
-	name: String;
+    // 이름
+    @ApiProperty({ description: '이름' })
+    @IsString()
+    @Column({ type: 'varchar', nullable: false, comment: '이름' })
+    name: String;
 
-	// 종목 코드
-	@IsNumberString()
-	@Column({ type: "numeric", nullable: false, comment: "종목 코드" })
-	code: String;
+    // 종목 코드
+    @ApiProperty({ description: '종목 코드' })
+    @IsString()
+    @IsNumber()
+    @Column({ type: 'varchar', nullable: false, comment: '종목 코드' })
+    code: String;
 
-	// // TODO -> enum type
-	// // 업종 카테고리
-	// @IsString()
-	// @Column({ type: "char", nullable: true, comment: "업종 카테고리 - 추후 enum으로 변경" })
-	// category: String;
+    // // TODO -> enum type
+    // // 업종 카테고리
+    // @IsString()
+    // @Column({ type: "char", nullable: true, comment: "업종 카테고리 - 추후 enum으로 변경" })
+    // category: String;
 
-	// 업종
-	@IsString()
-	@Column({ type: "varchar", comment: "업종" })
-	sectors: String;
+    // 업종
+    @ApiProperty({ description: '업종' })
+    @IsString()
+    @Column({ type: 'varchar', comment: '업종' })
+    sectors: String;
 
-	// 주요 제품
-	@IsString()
-	@Column({ type: "varchar", comment: "주요 제품" })
-	products: String;
+    // 주요 제품
+    @ApiProperty({ description: '주요 제품' })
+    @IsString()
+    @Column({ type: 'varchar', comment: '주요 제품' })
+    products: String;
 
-	// 상장일
-	@IsDate()
-	@Column({ type: "date", comment: "상장일" })
-	listingDate: Date;
+    // 상장일
+    @ApiProperty({ description: '상장일', type: Date })
+    @IsDate()
+    @Column({ type: 'date', comment: '상장일' })
+    listingDate: Date;
 
-	// 홈페이지
-	@IsUrl()
-	@Column({ type: "varchar", comment: "홈페이지", nullable: true })
-	url: String;
+    // 홈페이지
+    @ApiProperty({ description: '홈페이지' })
+    @IsUrl()
+    @Column({ type: 'varchar', comment: '홈페이지', nullable: true })
+    url: String;
 
-	// 지역
-	// TODO -> enum type
-	@IsString()
-	@Column({ type: "varchar", comment: "지역" })
-	location: String;
+    // 지역
+    // TODO -> enum type
+    @ApiProperty({ description: '지역' })
+    @IsString()
+    @Column({ type: 'varchar', comment: '지역' })
+    location: String;
 
-	@CreateDateColumn({ type: "datetime", precision: null, default: () => "CURRENT_TIMESTAMP" })
-	createdAt: Date;
+    @ApiProperty({ description: 'createAt' })
+    @CreateDateColumn({ type: 'datetime', precision: null, default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date;
 
-	@UpdateDateColumn({ type: "datetime", precision: null, nullable: true, default: () => "CURRENT_TIMESTAMP", onUpdate: "CURRENT_TIMESTAMP" })
-	updatedAt: Date;
+    @ApiProperty({ description: 'updatedAt' })
+    @UpdateDateColumn({
+        type: 'datetime',
+        precision: null,
+        nullable: true,
+        default: () => 'CURRENT_TIMESTAMP',
+        onUpdate: 'CURRENT_TIMESTAMP',
+    })
+    updatedAt: Date;
 }
