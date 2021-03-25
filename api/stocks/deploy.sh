86d66bfb15ae9aa0879a7fb73ae483349701cca5
kapri.kim <kaprikim@dreamyouns.com>
2021-03-25 17:42:40 +0900

    stock backend 세팅

---
+++ api/stocks/deploy.sh
@@ -0,0 +1,7 @@
+source config.sh
+
+echo "\x1B[93m[[ get-credentials ]]\x1B[0m"
+gcloud container clusters get-credentials ${CLUSTER_NAME} --zone ${ZONE} --project ${PROJECT_ID}
+
+gcloud builds submit --config cloudbuild.yaml --project ${PROJECT_ID} \
+--substitutions=_CLOUDRUNSTAMP=${CLOUDRUNSTAMP},_APP_NAME=${APP_NAME},_ZONE=${ZONE},_CLUSTER_NAME=${CLUSTER_NAME},_NAMESPACE=${NAMESPACE}
