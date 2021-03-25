d97214a1bf25defdf90b62bca09fa8f97e875a13
kapri.kim <kaprikim@dreamyouns.com>
2021-03-25 18:34:09 +0900

    health check 추가

--- api/stocks/src/main.ts
+++ api/stocks/src/main.ts
@@ -5,6 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    app.use("/", (req, res) => {
+        res.send("Ok")
+    })
+    
     // prefix : /api/v1
     app.setGlobalPrefix('api/v1');
 
