3c625c6c567e7179494c6af9866f90831c008d81
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-03-26 14:54:28 +0900

    yahoo finance 패키지 추가 및 기존 크롤링 코드 삭제

--- api/stocks/src/module/price/crawl.ts
+++
@@ -1,8 +0,0 @@
-import { HttpModule, Module } from '@nestjs/common';
-import { PriceInfo } from 'src/company/dto/price.dto';
-
-@Module({
-    imports: [HttpModule],
-    providers: [PriceInfo],
-})
-export class GetPriceInfoModule {}
