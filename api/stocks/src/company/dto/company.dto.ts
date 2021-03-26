6dc0cb0bc5b5db9c9822546958997ac444d22352
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-03-26 14:56:45 +0900

    commit

--- api/stocks/src/company/dto/company.dto.ts
+++ api/stocks/src/company/dto/company.dto.ts
@@ -1,20 +1,22 @@
 import { ApiProperty, getSchemaPath, IntersectionType, PartialType, PickType } from '@nestjs/swagger';
 import { IsObject, ValidateNested } from 'class-validator';
 import { Response } from 'src/common/dto/response.dto';
-import { Company } from 'src/entities/company.entity';
-import { PriceInfo } from './price.dto';
+import { Company, MarketType } from 'src/entities/company.entity';
+import { Price } from './price.dto';
 
 export class GetCompanyInfoResponse {
     @ApiProperty({ description: '회사 정보', type: Company })
     @ValidateNested()
     company: Company;
 
-    @ApiProperty({ description: '가격 정보', type: PriceInfo })
+    @ApiProperty({ description: '가격 정보', type: Price })
     @ValidateNested()
-    price: PriceInfo;
+    price: Price;
 }
 
-export class GetCompanyInfoBySearchDTO extends PickType(Company, ['name', 'products', 'sectors']) {
+export class GetCompanyInfoBySearchDTO extends PickType(Company, ['type', 'name', 'products', 'sectors']) {
+    @ApiProperty({ required: true })
+    type: MarketType;
     @ApiProperty({ required: false })
     name: string;
 
--- api/stocks/src/company/dto/price.dto.ts
+++ api/stocks/src/company/dto/price.dto.ts
@@ -1,43 +1,22 @@
 import { ApiPreconditionFailedResponse, ApiProperty, getSchemaPath } from '@nestjs/swagger';
 import { Response } from 'src/common/dto/response.dto';
 
-class Price {
+export class Price {
+    @ApiProperty({ description: '조회 일자', type: 'Date' })
+    date: Date;
+
     @ApiProperty({ description: '현재가' })
     now: Number;
 
-    @ApiProperty({
-        description: '전일 최종 정보',
-        properties: {
-            start: { type: 'number', description: '전일 시가' },
-            highest: { type: 'number', description: '전일 고가' },
-            lowest: { type: 'number', description: '전일 저가' },
-            upperLimit: { type: 'number', description: '전일 상한가' },
-            lowerLimit: { type: 'number', description: '전일 하한가' },
-        },
-    })
-    yesterday: {
-        start: Number;
-        highest: Number;
-        lowest: Number;
-        upperLimit: Number;
-        lowerLimit: Number;
-    };
-}
+    @ApiProperty({ description: '고가' })
+    high: Number;
 
-export class PriceInfo extends Price {
-    @ApiProperty({ description: '가격 조회 일자' })
-    datetime: Date;
+    @ApiProperty({ description: '저가' })
+    low: Number;
 
-    @ApiProperty({
-        description: '가격 정보',
-        type: Price,
-    })
-    price: Price;
+    @ApiProperty({ description: '전일 종가' })
+    previous: Number;
 
     @ApiProperty({ description: '거래량' })
     volume: Number;
-}
-
-export class PriceInfoResponse extends Response {
-    data?: PriceInfo;
 }
