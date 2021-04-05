5ff7ac3f559056e7c611f93c5aff741e982f31c9
Cobb Yang <41960405+GAFFWC@users.noreply.github.com>
2021-04-05 14:48:55 +0900

    redis service 생성

--- api/stocks-cron/src/cron/redis.service.ts
+++ api/stocks-cron/src/cron/redis.service.ts
@@ -3,26 +3,85 @@ import { ClientRedis } from '@nestjs/microservices';
 import { IsString } from 'class-validator';
 import { Subject } from 'rxjs';
 import { promisify } from 'util';
+import { GetCompanyInfoResponse } from './dto/info.dto';
 @Injectable({})
 export class RedisService {
     constructor(@Inject('redis') private readonly redis: ClientRedis) {}
 
     private readonly client = this.redis.createClient(new Subject<Error>());
 
-    ping() {
-        return this.client.ping();
+    async ping() {
+        const pingAsync = promisify(this.client.ping).bind(this.client);
+        return await pingAsync();
     }
 
-    async get(key: string) {
+    async get(key: string): Promise<string> {
         const getAsync = promisify(this.client.get).bind(this.client);
 
-        console.log(await getAsync(key));
-        this.client.get(key, (err, res) => {
-            if (err) {
-                console.error(err);
-            }
+        try {
+            const value = await getAsync(key);
+            return JSON.parse(value);
+        } catch (err) {
+            console.error(err);
+        }
+    }
 
-            return res;
-        });
+    async set(key: string, value: GetCompanyInfoResponse): Promise<boolean> {
+        const setAsync = promisify(this.client.set).bind(this.client);
+
+        try {
+            await setAsync(key, JSON.stringify(value));
+            return true;
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    async lset(key: string, values: GetCompanyInfoResponse[]): Promise<boolean> {
+        const lpushAsync = promisify(this.client.lpush).bind(this.client);
+
+        try {
+            for await (const value of values) {
+                lpushAsync(key, JSON.stringify(value));
+            }
+            return true;
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    async lrange(key: string, start: number, end: number): Promise<any[]> {
+        const lrangeAsync = promisify(this.client.lrange).bind(this.client);
+
+        try {
+            const list = await lrangeAsync(key, start, end);
+
+            return list.map((value: string) => {
+                return JSON.parse(value);
+            });
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    async del(key: string): Promise<boolean> {
+        const delAsync = promisify(this.client.del).bind(this.client);
+
+        try {
+            await delAsync(key);
+            return true;
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    async llen(key: string): Promise<number> {
+        const llenAsync = promisify(this.client.llen).bind(this.client);
+
+        try {
+            return await llenAsync(key);
+        } catch (err) {
+            console.error(err);
+        }
     }
 }
